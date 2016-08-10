8c0b6b1751ddb5a9ffcf53860af66ea90dd79f40
杨军 <yangjunstone@gmail.com>
2016-08-10 14:32:56 +0800

    rename all item input ids and names. still broken

--- functional_tests/test_layout_and_styling.py
+++ functional_tests/test_layout_and_styling.py
@@ -7,7 +7,7 @@ class LayoutAndStylingTest(FunctionalTest):
         self.browser.set_window_size(1024, 768)
 
         #她看到输入框完美地居中显示
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         #inputbox.send_keys('testing\n')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width']/2,
--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -6,7 +6,7 @@ from lists.forms import EMPTY_LIST_ERROR, ItemForm
 class ItemFormTest(TestCase):
     def test_form_renders_item_text_input(self):
         form = ItemForm()
-        self.fail(form.as_p())
+        print(form.as_p())
 
     def test_form_item_input_has_placeholder_and_css_classes(self):
         form = ItemForm()
