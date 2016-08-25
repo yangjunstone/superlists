6a4950409b52182d66c3fed49ea8f636eadcb36c
杨军 <yangjunstone@gmail.com>
2016-08-25 09:38:33 +0800

    new_list view tries to assign owner but cant

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -25,13 +25,36 @@ class MylistsTest(FunctionalTest):
         ))
 
     def test_logged_in_user_lists_are_saved_as_my_lists(self):
-        email = 'edith@example.com'
-
-        self.browser.get(self.server_url)
-        self.wait_to_be_logged_out(email)
-
         # 伊迪丝是已登录用户
-        self.create_pre_authenticated_session(email)
+        self.create_pre_authenticated_session('edith@example.com')
 
+        # 她访问首页, 新建一个清单
         self.browser.get(self.server_url)
-        self.wait_to_be_logged_in(email)
+        self.get_item_input_box().send_keys('Reticulate splines\n')
+        self.get_item_input_box().send_keys('Immanentize eschaton\n')
+        first_list_url = self.browser.current_url
+
+        # 她第一次看到My Lists链接
+        self.browser.find_element_by_link_text('My Lists').click()
+
+        # 她看到这个页面中有她创建的清单
+        # 而且清单根据第一个待办事项命名
+        self.browser.find_element_by_link_text('Reticulate splines').click()
+        self.assertEqual(self.browser.current_url, first_list_url)
+
+        # 她决定再建一个清单试试
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('Click cows\n')
+        second_list_url = self.browser.current_url
+
+        # 在My List页面, 这个新建的清单也显示出来了
+        self.browser.find_element_by_link_text('My lists').click()
+        self.browser.find_element_by_link_text('Click cows').click()
+        self.assertEqual(self.browser.current_url, second_list_url)
+
+        # 她退出后, My Lists链接不见了
+        self.browser.find_element_by_id('id_logout').click()
+        self.assertEqual(
+            self.browser.find_elements_by_link_text('My lists'),
+            []
+        )
