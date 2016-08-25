ff58014d80b7b7b9786998364e8d5ba0e2d37a49
杨军 <yangjunstone@gmail.com>
2016-08-25 09:15:35 +0800

    url, placeholder view, and first-cut templates for my_lists

--- lists/urls.py
+++ lists/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import url
+from lists import views
 
 urlpatterns = [
-    url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
-    url(r'^new$', 'lists.views.new_list', name='new_list'),
+    url(r'^(\d+)/$', views.view_list, name='view_list'),
+    url(r'^new$', views.new_list, name='new_list'),
+    url(r'^users/(.+)/$', views.my_lists, name='my_lists'),
 ]
